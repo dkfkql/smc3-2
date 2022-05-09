d40676807ccbdea293d4206d42c11c98d19c4481
Administrator <Administrator@501-S26>
2022-05-09 10:20:53 +0900

    dfg

--- 0509/src/Number.java
+++ 0509/src/Number.java
@@ -8,6 +8,22 @@ public class Number {
         int num=sc.nextInt();
 
         int []arr=new int[10];
+        int diff = 0 ;
+        for(int i=0; i<10; i++) {
+        	arr [i] = (int)(Math.random()*50+1);
+        	System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        for(int i=0; i<10; i++) {
+       if(arr[i] == num) {
+    	   System.out.println("당첨! 일치하는 숫자가 있습니다");
+    	   diff=1;
+       }
+        }
+        if(diff==0) System.out.println( "꽈!다음기회에~");
+        //반복문+
+        
+ 
         
 
         
